c54f8054816edf586f1dc669e7742b9df3072cd9
Per Enström <enstrom.per@gmail.com>
2022-09-20 16:19:04 +0200

    use index as logged in route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,16 @@
-import React from 'react';
 import { NextPage } from 'next';
-import Head from 'next/head';
-import { Container, Box, Paper, Typography } from '@mui/material';
+import {
+  useUser,
+  withPageAuthRequired,
+  WithPageAuthRequiredProps
+} from '@auth0/nextjs-auth0';
 
-const IndexPage: NextPage<{}> = ({}) => {
-  return (
-    <Container maxWidth="md">
-      <Head>
-        <title>NextJS Typescript Starter</title>
-      </Head>
-      <Box mt={6}>
-        <Paper>
-          <Box p={2}>
-            <Typography variant={'h1'}>
-              Opinionated NextJS Typescript starter
-            </Typography>
-            <Typography variant={'subtitle1'}>With Material-UI</Typography>
-            <p>
-              This is my preferred starter template for building NextJS apps in
-              Typescript. This version also includes{' '}
-              <a href="https://mui.com/">MUI</a> (formerly known as Material UI)
-              for quicker prototyping.
-            </p>
-          </Box>
-        </Paper>
-      </Box>
-    </Container>
-  );
+interface Props {}
+
+const IndexPage: NextPage<Props> = ({}) => {
+  const { user } = useUser();
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
 };
 
-export default IndexPage;
+export default withPageAuthRequired<Props & WithPageAuthRequiredProps>(IndexPage);
