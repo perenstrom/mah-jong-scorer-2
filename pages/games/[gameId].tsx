9c4960fa33409931d7bc27e557edb561f67cb46f
Per Enström <enstrom.per@gmail.com>
2023-01-17 21:18:21 +0100

    WIP

--- pages/games/[gameId].tsx
+++ pages/games/[gameId].tsx
@@ -2,7 +2,7 @@ import {
   withPageAuthRequired,
   WithPageAuthRequiredProps
 } from '@auth0/nextjs-auth0';
-import { Box, Button, Stack, styled, Typography } from '@mui/joy';
+import { Box, Stack, styled, Typography } from '@mui/joy';
 import { calculateStandings } from 'helpers/gameHelper';
 import { calculateResults } from 'helpers/transactionHelper';
 import { prismaContext } from 'lib/prisma';
@@ -12,11 +12,75 @@ import React from 'react';
 import { getExpandedGame } from 'services/prisma/games';
 import { getTransactions } from 'services/prisma/transactions';
 import { ExpandedGame, Transaction, TransactionResult } from 'types/types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCrown, faWind } from '@fortawesome/free-solid-svg-icons';
 
 const TableBodyRow = styled('tr')`
   &:nth-child(odd) {
     background-color: #f1f6ff;
   }
+`;
+
+// TODO: FORTSÄTT STYLA LÄGG TILL POÄNG FÖR EN RUNDA
+
+const ScoreInput = styled('input')`
+  border: 0;
+  font-size: 2rem;
+  min-width: 0;
+  padding-left: 0.5rem;
+`;
+
+const ScoreAvatar: React.FC<{ initials: string; color: string }> = ({
+  initials,
+  color
+}) => (
+  <Box
+    sx={{
+      flex: '0 0 3rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: color
+    }}
+  >
+    {initials}
+  </Box>
+);
+
+const WindSelector: React.FC<{}> = () => (
+  <Box
+    sx={{
+      flex: '0 0 3rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderLeft: '1px solid #eee'
+    }}
+  >
+    <FontAwesomeIcon icon={faWind} />
+  </Box>
+);
+
+const WinnerSelector: React.FC<{}> = () => (
+  <Box
+    sx={{
+      flex: '0 0 3rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderLeft: '1px solid #eee'
+    }}
+  >
+    <FontAwesomeIcon icon={faCrown} />
+  </Box>
+);
+
+const SaveButton = styled('button')`
+  width: 100%;
+  height: 100%;
+  border-radius: 0;
+  border: 0;
+  font-size: 1.2rem;
 `;
 
 const LeaderBoardItem: React.FC<{ name: string; points: number }> = ({
@@ -70,7 +134,7 @@ const GameDetailsPage: NextPage<Props> = ({ game, transactions }) => {
 
   return (
     <>
-      <Stack direction="row">
+      <Stack direction="row" sx={{ height: '15rem' }}>
         <Stack sx={{ flexBasis: '20rem', m: 0, p: 0 }} component="ul">
           {leaderboard.map((leaderBoardItem) => (
             <LeaderBoardItem
@@ -99,15 +163,57 @@ const GameDetailsPage: NextPage<Props> = ({ game, transactions }) => {
         >
           Graph under construction
         </Box>
-        <Stack sx={{ backgroundColor: 'peachpuff', flexBasis: '20rem' }}>
-          <Box sx={{ flexGrow: '1' }}>{game.players.player1.user?.name}</Box>
-          <Box sx={{ flexGrow: '1' }}>{game.players.player2.user?.name}</Box>
-          <Box sx={{ flexGrow: '1' }}>{game.players.player3.user?.name}</Box>
-          <Box sx={{ flexGrow: '1' }}>{game.players.player4.user?.name}</Box>
-          <Box sx={{ flexGrow: '1' }}>
-            <Button variant="plain" sx={{ width: '100%' }}>
-              Save
-            </Button>
+        <Stack sx={{ flexBasis: '20rem', minWidth: '0' }}>
+          <Stack
+            sx={{ flex: '1', borderBottom: '1px solid #eee' }}
+            direction="row"
+          >
+            <ScoreAvatar
+              initials={game.players.player1.user?.name.substring(0, 1) || ''}
+              color="#F26419"
+            />
+            <ScoreInput value="0" />
+            <WindSelector />
+            <WinnerSelector />
+          </Stack>
+          <Stack
+            sx={{ flex: '1', borderBottom: '1px solid #eee' }}
+            direction="row"
+          >
+            <ScoreAvatar
+              initials={game.players.player2.user?.name.substring(0, 1) || ''}
+              color="#21C46D"
+            />
+            <ScoreInput value="0" />
+            <WindSelector />
+            <WinnerSelector />
+          </Stack>
+          <Stack
+            sx={{ flex: '1', borderBottom: '1px solid #eee' }}
+            direction="row"
+          >
+            <ScoreAvatar
+              initials={game.players.player3.user?.name.substring(0, 1) || ''}
+              color="#33658A"
+            />
+            <ScoreInput value="0" />
+            <WindSelector />
+            <WinnerSelector />
+          </Stack>
+          <Stack
+            sx={{ flex: '1', borderBottom: '1px solid #eee' }}
+            direction="row"
+          >
+            <ScoreAvatar
+              initials={game.players.player4.user?.name.substring(0, 1) || ''}
+              color="#F6AE2D"
+            />
+            <ScoreInput value="0" />
+            <WindSelector />
+            <WinnerSelector />
+          </Stack>
+          <Box sx={{ flex: '1' }}>
+            <SaveButton>Save</SaveButton>
           </Box>
         </Stack>
       </Stack>
