8b6d356ae766c5f632ed22fb9f6243b3ad7f0ad5
Per Enström <enstrom.per@gmail.com>
2023-02-08 19:58:44 +0100

    Add getUser helper

--- lib/authorization.ts
+++ lib/authorization.ts
@@ -1,13 +1,17 @@
 import { UserProfile, getSession } from '@auth0/nextjs-auth0';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+export const getUser = (req: NextApiRequest, res: NextApiResponse) => {
+  const session = getSession(req, res);
+  return session?.user as UserProfile | undefined;
+};
+
 export const isAuthorized = (
   req: NextApiRequest,
   res: NextApiResponse,
   playerId: string
 ) => {
-  const session = <{ user: UserProfile }>getSession(req, res);
-  const user = session?.user ?? null;
+  const user = getUser(req, res);
 
   return user ? user?.sub === playerId : false;
 };
